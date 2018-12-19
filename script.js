5f67a0b1dbab2f39dbaf606f97039ada78ca5b7b
maryapple <maria9ku@gmail.com>
2018-12-19 14:46:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+menu.onclick = function myFunction(){
+	// x - блок с идентификатором topnav
+	var x = document.getElementById("myTopnav");
+
+	if(x.className === "topnav")
+	{
+		x.className += " responsive";
+	}
+	else
+	{
+		x.className = "topnav";
+	}
+}
